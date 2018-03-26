d573753b63fead7f858d8ab771f7e5e36db8eace
Jeremias Enriquez <capuche777@gmail.com>
2018-03-25 20:35:39 -0600

    the data base is getting the users regitration

--- routes/users.js
+++ routes/users.js
@@ -3,53 +3,70 @@ var router = express.Router();
 var multer = require('multer');
 var upload = multer({dest: './uploads'});
 
+var User = require('../models/user');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
 router.get('/register', function(req, res, next) {
-  res.render('register', {title: 'Register'});
+  res.render('register',{title:'Register'});
 });
 
 router.get('/login', function(req, res, next) {
-  res.render('login', {title: 'Login'});
+  res.render('login', {title:'Login'});
 });
 
-router.post('/register', upload.single('profileimage'), function(req, res, next) {
-  let name = req.body.name;
-  let email = req.body.email;
-  let username = req.body.username;
-  let password = req.body.password;
-  let password2 = req.body.password2;
-  
-  if (req.file) {
-    console.log('Uploading File...')
-    let profileimage = req.file.filename;
+router.post('/register', upload.single('profileimage') ,function(req, res, next) {
+  var name = req.body.name;
+  var email = req.body.email;
+  var username = req.body.username;
+  var password = req.body.password;
+  var password2 = req.body.password2;
+
+  if(req.file){
+  	console.log('Uploading File...');
+  	var profileimage = req.file.filename;
   } else {
-    console.log('No File Uploaded...')
-    let profileimage = 'noimage.jpg'
+  	console.log('No File Uploaded...');
+  	var profileimage = 'noimage.jpg';
   }
 
-  //Form validatod
-  req.checkBody('name', 'Name field is required').notEmpty();
-  req.checkBody('email', 'Email field is required').notEmpty();
-  req.checkBody('email', 'Email is not valid').isEmail();
-  req.checkBody('username', 'Username field is required').notEmpty();
-  req.checkBody('password', 'Password field is required').notEmpty();
-  req.checkBody('password2', 'Password do not match').equals(req.body.password);
+  // Form Validator
+  req.checkBody('name','Name field is required').notEmpty();
+  req.checkBody('email','Email field is required').notEmpty();
+  req.checkBody('email','Email is not valid').isEmail();
+  req.checkBody('username','Username field is required').notEmpty();
+  req.checkBody('password','Password field is required').notEmpty();
+  req.checkBody('password2','Passwords do not match').equals(req.body.password);
 
-  //Check Errors
-  let errors = req.validationErrors();
+  // Check Errors
+  var errors = req.validationErrors();
 
-  if (errors) {
-    res.render('register', {
-      errors: errors
+  if(errors){
+  	res.render('register', {
+  		errors: errors
+  	});
+  } else{
+  	var newUser = new User({
+      name: name,
+      email: email,
+      username: username,
+      password: password,
+      profileimage: profileimage
     });
-  } else {
-    console.log('No Errors');
-  }
 
+    User.createUser(newUser, function(err, user){
+      if(err) throw err;
+      console.log(user);
+    });
+
+    req.flash('success', 'You are now registered and can login');
+
+    res.location('/');
+    res.redirect('/');
+  }
 });
 
 module.exports = router;
